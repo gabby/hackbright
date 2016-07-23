b4bdf9b4e31fb21e83f9cb2ccf7dcf1c7d118d16
Gabrielle Sin <gabriellesin@Sindication.local>
2016-07-23 16:32:43 -0700

    typos

--- game_of_fours.py
+++ game_of_fours.py
@@ -1,4 +1,4 @@
 zero_string='44-44'
-print "%s = %i" % (zer_string, eval(zero_string))
+print "%s = %i" % (zero_string, eval(zero_string))
 one_string="44/44"
-print "%s = %i" (one_string, eval(one_string)) 
+print "%s = %i" % (one_string, eval(one_string)) 
