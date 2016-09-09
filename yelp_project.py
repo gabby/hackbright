3d77e1036bba955618552dee1f1dba1eecfb376f
Gabby Sin <gabby@shyp.com>
2016-09-09 15:52:26 -0700

    draft 1 for hb final project

--- yelp_project.py
+++ yelp_project.py
@@ -12,36 +12,49 @@ with open('config_secret.json') as cred:
     client = Client(auth)
 
 params = {
-    'location': 'Argentina',
-    'limit': 19
-}
-
-response = client.search(**params)
-
+ 	"limit": 20,
+ 	"location": "London, UK",
+ 	"term":"hotdogs"
+}   
 #print response
 #print response.businesses[18].name, response.businesses[18].location.display_address, response.businesses[18].display_phone
-print response.businesses
+#print response.businesses
 #print response.businesses[0].location.display_address
 
 #location, count, search_term=""
-# def print_biz_info():
-# 	for entry in response.businesses:
-# 		print response.businesses[entry].name
-# 		print response.businesses[entry].location.display_address
-# 		print response.businesses[entry].display_phone
+# def add_params(keyword):
+# 	params["term"] = keyword
+# 	return params 
 
-# def main():
-# 	print print_biz_info()
+response = client.search(**params)
 
-# if __name__ == '__main__':
-# 	main()
-# for address in 
+def biz_info(num):
+	for entry in range(num):
+		print response.businesses[entry].name, response.businesses[entry].display_phone
+		print response.businesses[entry].location.display_address
+
+def main():
+	# search_term = raw_input("What do you want to search for: ")
+	# print add_params(search_term)
+	num_entries = int(raw_input("How many entries do you need? "))
+	print "\n"	
+	print biz_info(num_entries)
+
+
+if __name__ == '__main__':
+	main()
+ 
 # # #print pprint(dir(response))
 
 #  1. Get input for Location, Count, search_term
 #  2. Append params dictionary with Key and values matching Yelp APi documentation and raw inputs 
 #  3. reassign that params  dictionary for the `response` variable 
 
+
+
+	# for info in all_contacts:
+	# 		if info.first_name == fname and info.last_name == lname:
+	# 			print(info.first_name, info.last_name, info.mobile_phone, info.home_p
 
 
 
